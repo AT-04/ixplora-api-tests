e62dae2d299eab999c09c3067f91af25b3760ac7
JoseTorrez <juanjo.torrez@fundacion-jala.org>
2017-10-03 16:41:30 -0400

    Adding the report_builder configuration

---
+++ features/lib/report_builder.rb
@@ -0,0 +1,16 @@
+# Author: Jose Torrez
+module ReportBuilder
+  def self.configure
+    defaults = builder.default_options
+    yield defaults if block_given?
+    builder.options = defaults.marshal_dump
+  end
+
+  def self.build_report(options = nil)
+    builder.build_report(options)
+  end
+
+  def self.builder
+    @builder ||= Builder.new
+  end
+end
