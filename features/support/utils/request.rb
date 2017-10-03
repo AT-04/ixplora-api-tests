210a5593361e0507640820ae48b5596713da5612
marcos-x86 <marcos.x86@outlook.com>
2017-10-03 17:02:30 -0400

    Helper and Endpoint builder implemented

--- features/support/utils/request.rb
+++ features/support/utils/request.rb
@@ -2,14 +2,23 @@ require 'json'
 
 # Request class that abstracts the properties and fields of a API Request.
 class Request
-  attr_reader :method, :header, :endpoint
-  attr_accessor :body
+  attr_reader :method, :header, :endpoint, :body
 
-  def initialize(method:, endpoint:, token: '', body: '')
+  def initialize(method)
     @method = method
-    @body = body.to_json
+    @body = {}
     @header = { :'Content-type' => 'application/json' }
-    @header['Authorization'] = "Bearer #{token}" unless token.strip.empty?
-    @endpoint = "#{$server_host}:#{$server_port}#{$server_extension}#{$server_version}#{endpoint}"
+  end
+
+  def add_authorization(token)
+    @header['Authorization'] = "Bearer #{token}"
+  end
+
+  def add_body(content)
+    @body = JSON.parse(content)
+  end
+
+  def append_endpoint(endpoint)
+    @endpoint = CommonActions.built_endpoint("#{$server_base_endpoint}#{endpoint}")
   end
 end
